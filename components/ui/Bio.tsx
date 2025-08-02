ab6e2360a0ec65b315a335edd0d6aad55708dd68
kayf <kayfgithub@gmail.com>
2025-08-01 22:22:46 -0300

    feat: add bio

--- components/ui/Bio.tsx
+++ components/ui/Bio.tsx
@@ -1,7 +1,15 @@
-
 export default function Bio() {
   return (
-    <div className="">
+    <div className="w-full h-80 flex justify-start items-center flex-col">
+      <span className="self-start text-white font-bold underline decoration-4 decoration-zinc-600 underline-offset-6">
+        Bio
+      </span>
+      <div className="self-start text-md text-white my-4">
+        <strong className="mr-4">1984</strong>Born in Osaka (大阪), Japan. <br />
+        <strong className="mr-4">2010</strong>Completed the Master&apos;s Program in the Graduate School of Information Science at Nara Institute of Science and Technology (奈良先端科学技術大学院大学情報科学研究科修士課程) <br />
+        <strong className="mr-4">2010</strong>Worked at Yahoo! Japan (ヤフー株式会社入社) <br />
+        <strong className="mr-4">2012 to present</strong>Working as a freelancer <br />
+      </div>
     </div>
   )
 }
